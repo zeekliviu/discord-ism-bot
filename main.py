d885cf234daeadb073fb4d75c96a75a7e0f8b5f8
Zeek Liviu <89124675+zeekliviu@users.noreply.github.com>
2025-02-12 10:34:51 +0200

    fix: fixed the persistence of the dialog between bot restarts

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import discord
 from discord.ext import commands
-from os import getenv
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -11,7 +10,8 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 class RoleButton(discord.ui.Button):
     def __init__(self, label, role_id):
-        super().__init__(label=label, style=discord.ButtonStyle.primary)
+        custom_id = f"role_button:{role_id}:{label}"
+        super().__init__(label=label, style=discord.ButtonStyle.primary, custom_id=custom_id)
         self.role_id = role_id
 
     async def callback(self, interaction: discord.Interaction):
@@ -23,6 +23,12 @@ class RoleButton(discord.ui.Button):
             else:
                 await interaction.response.send_message(f"You are already part of {role.name}!", ephemeral=True)
 
+ROLES = {
+        "1137": 1288142856674086943,
+        "1138": 1288142664625295421,
+        "Visitor": 1302953783575187547
+}
+
 class RoleView(discord.ui.View):
     def __init__(self, roles):
         super().__init__(timeout=None)
@@ -31,16 +37,12 @@ class RoleView(discord.ui.View):
 
 @bot.command()
 async def role_buttons(ctx):
-    roles = {
-        "1137": 1288142856674086943,
-        "1138": 1288142664625295421,
-        "Visitor": 1302953783575187547
-    }
-    view = RoleView(roles)
+    view = RoleView(ROLES)
     await ctx.send("Choose the role you want:", view=view)
 
 @bot.event
 async def on_ready():
+    bot.add_view(RoleView(ROLES))
     print(f'Logged in as {bot.user.name} ({bot.user.id})')
 
-bot.run(getenv("BOT_SECRET"))
+bot.run('BOT_SECRET')
